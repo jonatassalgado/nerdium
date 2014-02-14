6c888a33052da95f77f9dbb3e95bb89eb6d8a4e5
Jonatas Eduardo <jottaeblog@gmail.com>
2014-02-14 06:01:44 -0200

    Criação de testes para a tabela de usuários

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+  					 uniqueness: true,
+  					 format: {
+  					 		with: /\A[a-zA-Z0-9]+\Z/,
+  					 		message: "O nome do perfil nao pode ter espacos."
+  					 }	
+
+  validates_length_of :profile_name, minimum: 3, too_short: "O nome de usuarios precisa ter mais de 2 caracteres."
+
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,62 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+  test "a user should enter a first name" do
+  	user = User.new
+
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+  	user = User.new
+
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+  	user = User.new
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+  	user = User.new
+  	user.profile_name = users(:user_block).profile_name
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+	end
+
+  test "a user should have a unique email" do
+    user = User.new
+    user.email = users(:user_block).email
+
+    assert !user.save
+    assert !user.errors[:email].empty?
+  end
+
+  test "a user should enter a profile name unless or equal then 3 characters" do
+    user = User.new
+    user.profile_name = 12
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("O nome de usuarios precisa ter mais de 2 caracteres.")
+  end
+
+	test "a user should have a profile name without spaces" do
+		user = User.new
+		user.profile_name = "user profile name with spaces"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("O nome do perfil nao pode ter espacos.")
+	end
+
+
 end
