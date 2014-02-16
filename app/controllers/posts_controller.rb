3d9604ee24da5da57a195e784394e11d1ed4e9ce
Jonatas Eduardo <jottaeblog@gmail.com>
2014-02-15 23:25:02 -0200

    Criação de posts com identificação do user logado via current_user do Devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   # GET /posts
   # GET /posts.json
@@ -25,7 +26,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
