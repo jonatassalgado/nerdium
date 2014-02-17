511935983e60a5ded15737eab88fd9dd10eb3271
Jonatas Eduardo <jottaeblog@gmail.com>
2014-02-17 16:05:55 -0300

    Configuração da Role Admin via Devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216073013) do
+ActiveRecord::Schema.define(version: 20140217184607) do
 
   create_table "documents", force: true do |t|
     t.integer  "user_id"
@@ -59,12 +59,12 @@ ActiveRecord::Schema.define(version: 20140216073013) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "profile_name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20140216073013) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "slug"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
