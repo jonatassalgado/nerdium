d8146e70b7c345f26393fb629b6fc2b700a7b90e
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2014-11-27 18:42:48 -0200

    Adjust in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create(
+    first_name: 'John',
+    last_name: 'Doe',
+    profile_name: 'john',
+    email: 'admin@gmail.com',
+    password: 'password',
+    password_confirmation: 'password',
+    admin: true
+)
+
+puts "======= ADMIN ACCESS ======="
+puts "/n"
+puts "Admin created with:"
+puts "email: admin@gmail.com"
+puts "password: password"
+puts "/n"
+puts "Access: localhost:3000/entrar"
